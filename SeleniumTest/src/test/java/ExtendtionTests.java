f22e0958db961a1c26dc808df27ba6793b1354fa
Druuuz <olesov198@gmail.com>
2018-07-17 23:52:38 +0300

    xcvxc

--- SeleniumTest/src/test/java/ExtendtionTests.java
+++ SeleniumTest/src/test/java/ExtendtionTests.java
@@ -26,28 +26,38 @@ public class ExtendtionTests {
         driver.quit();
     }
 
+    /*
+        При клике по ссылке с текстом «ENABLE AUTO REFRESH» эта ссылка пропадает,
+        а вместо неё появляется ссылка с текстом «DISABLE AUTO REFRESH».
+        При клике по ссылке с текстом «DISABLE AUTO REFRESH» эта ссылка пропадает,
+        а вместо неё появляется ссылка с текстом «ENABLE AUTO REFRESH».
+     */
     @Test
     public void testRefresh() {
         driver.get(base_url);
         AuthPage authPage = PageFactory.initElements(driver, AuthPage.class);
         authPage.auth("user", "password");
         RefreshArea refreshArea = PageFactory.initElements(driver, RefreshArea.class);
-        Assert.assertTrue(refreshArea.autoRefreshLinkExists(),"AutoRefresh link does not exist");
+        Assert.assertTrue(refreshArea.autoRefreshLinkExists(), "AutoRefresh link does not exist");
         refreshArea.clickAutoRefreshLink();
-        Assert.assertFalse(refreshArea.autoRefreshLinkExists(),"AutoRefresh link does not disappeared");
-        Assert.assertTrue(refreshArea.disAutoRefreshLinkExists(),"DisAutoRefresh link does not exist");
+        Assert.assertFalse(refreshArea.autoRefreshLinkExists(), "AutoRefresh link does not disappeared");
+        Assert.assertTrue(refreshArea.disAutoRefreshLinkExists(), "DisAutoRefresh link does not exist");
         refreshArea.clickDisAutoRefreshLink();
-        Assert.assertFalse(refreshArea.disAutoRefreshLinkExists(),"DisAutoRefresh link does not disappeared");
-        Assert.assertTrue(refreshArea.autoRefreshLinkExists(),"AutoRefresh link does not exist");
+        Assert.assertFalse(refreshArea.disAutoRefreshLinkExists(), "DisAutoRefresh link does not disappeared");
+        Assert.assertTrue(refreshArea.autoRefreshLinkExists(), "AutoRefresh link does not exist");
     }
 
+    /*
+    	При попытке создать пользователя с пустым (незаполненным) именем
+    	на странице появляется текст «"" is prohibited as a full name for security reasons.»
+     */
     @Test
     public void testAuth() {
         driver.get(base_url);
         AuthPage authPage = PageFactory.initElements(driver, AuthPage.class);
         authPage.auth("", "password");
         IncorrectAuthPage incorrectAuthPage = PageFactory.initElements(driver, IncorrectAuthPage.class);
-        Assert.assertTrue(incorrectAuthPage.existsText("is prohibited as a full name for security reasons."),"Page does not exist text");
+        Assert.assertTrue(incorrectAuthPage.existsText("is prohibited as a full name for security reasons."), "Page does not exist text");
     }
 
 }
--- SeleniumTest/src/test/java/Tests.java
+++ SeleniumTest/src/test/java/Tests.java
@@ -10,14 +10,12 @@ import org.testng.annotations.Test;
 public class Tests {
 
     String base_url = "http://localhost:8080/";
-    StringBuffer verificationErrors = new StringBuffer();
     WebDriver driver = null;
 
     @BeforeClass
     public void beforeClass() throws Exception {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Downloads\\chromedriver.exe");
         driver = new ChromeDriver();
-
     }
 
     @AfterClass
@@ -25,6 +23,9 @@ public class Tests {
         driver.quit();
     }
 
+    /*
+   0.	После ввода авторизационных данных происходит переход на главную страницу сайта.
+    */
     @Test
     public void testAuth() {
         driver.get(base_url);
@@ -32,9 +33,13 @@ public class Tests {
         authPage.auth("user", "password");
         MainJenkinsPage mainJenkinsPage = PageFactory.initElements(driver, MainJenkinsPage.class);
 
-        Assert.assertTrue(mainJenkinsPage.isLoggedIn(),"Not logged in");
+        Assert.assertTrue(mainJenkinsPage.isLoggedIn(), "Not logged in");
     }
 
+    /*
+    1.	После клика по ссылке «Manage Jenkins» на странице появляется элемент dt с текстом «Manage Users»
+        и элемент dd с текстом «Create/delete/modify users that can log in to this Jenkins».
+     */
     @Test(dependsOnMethods = "testAuth")
     public void testManageJenkins() {
         MainJenkinsPage mainJenkinsPage = PageFactory.initElements(driver, MainJenkinsPage.class);
@@ -45,6 +50,10 @@ public class Tests {
         Assert.assertTrue(manageJenkinsPage.tagDtWithTextExists(), "Tag dt doesn't contain text");
     }
 
+    /*
+    2.	После клика по ссылке, внутри которой содержится элемент dt с текстом «Manage Users»,
+        становится доступна ссылка «Create User».
+     */
     @Test(dependsOnMethods = "testManageJenkins")
     public void testManageUsers() {
         ManageJenkinsPage manageJenkinsPage = PageFactory.initElements(driver, ManageJenkinsPage.class);
@@ -54,6 +63,10 @@ public class Tests {
         Assert.assertTrue(manageUsersPage.createUserLinkEnabled(), "User Link isn't enabled");
     }
 
+    /*
+    3.	После клика по ссылке «Create User» появляется форма с тремя полями типа text и двумя полями типа password,
+        причём все поля должны быть пустыми.
+     */
     @Test(dependsOnMethods = "testManageUsers")
     public void testCreateUser() {
         ManageUsersPage manageUsersPage = PageFactory.initElements(driver, ManageUsersPage.class);
@@ -63,6 +76,12 @@ public class Tests {
         Assert.assertTrue(createUserPage.formExists(), "Form does not exist");
     }
 
+    /*
+    4.	После заполнения полей формы («Username» = «someuser», «Password» = «somepassword»,
+        «Confirm password» = «somepassword», «Full name» = «Some Full Name», «E-mail address» = «some@addr.dom») и
+        клика по кнопке с надписью «Create User» на странице появляется строка таблицы (элемент tr),
+         в которой есть ячейка (элемент td) с текстом «someuser».
+     */
     @Test(dependsOnMethods = "testCreateUser")
     public void testExistingCreatedUser() {
         ManageUsersPage manageUsersPage = PageFactory.initElements(driver, ManageUsersPage.class);
@@ -72,6 +91,11 @@ public class Tests {
         Assert.assertTrue(manageUsersPage.userCreated(), "Created user isn't exist");
     }
 
+
+    /*
+    5.	После клика по ссылке с атрибутом href равным «user/someuser/delete»
+        появляется текст «Are you sure about deleting the user from Jenkins?».
+     */
     @Test(dependsOnMethods = "testExistingCreatedUser")
     public void testDeletingMessage() {
         ManageUsersPage manageUsersPage = PageFactory.initElements(driver, ManageUsersPage.class);
@@ -81,6 +105,21 @@ public class Tests {
         Assert.assertTrue(deletePage.existsText("Are you sure about deleting the user from Jenkins?"), "Text does not exist on page");
     }
 
+    /*
+    6.	После клика по кнопке с надписью «Yes» на странице отсутствует строка таблицы (элемент tr),
+        с ячейкой (элемент td) с текстом «someuser».
+        На странице отсутствует ссылка с атрибутом href равным «user/someuser/delete».
+     */
+    @Test(dependsOnMethods = "testDeletedUser")
+    public void testNotExistingLink() {
+        ManageUsersPage manageUsersPage = PageFactory.initElements(driver, ManageUsersPage.class);
+        Assert.assertFalse(manageUsersPage.someUserDeleteLinkExists(), "User delete link exists");
+    }
+
+    /*
+    7.	{На той же странице, без выполнения каких бы то ни было действий}.
+        На странице отсутствует ссылка с атрибутом href равным «user/admin/delete».
+     */
     @Test(dependsOnMethods = "testDeletingMessage")
     public void testDeletedUser() {
         DeletePage deletePage = PageFactory.initElements(driver, DeletePage.class);
@@ -89,12 +128,6 @@ public class Tests {
         deletePage.clickOnButtonYes();
         Assert.assertFalse(manageUsersPage.userCreated(), "User wasn't delete");
         Assert.assertFalse(manageUsersPage.adminDeleteLinkExists(), "Admin delete link exists");
-    }
-
-    @Test(dependsOnMethods = "testDeletedUser")
-    public void testNotExistingLink() {
-        ManageUsersPage manageUsersPage = PageFactory.initElements(driver, ManageUsersPage.class);
-        Assert.assertFalse(manageUsersPage.someUserDeleteLinkExists(), "User delete link exists");
     }
 
 
